402205dcd701cda077bf8fcc4886cac71fca4dc4
maksim.titov <gaiiiuiiialikeaboss@gmail.com>
2022-07-25 20:48:27 +0300

    new_app - создание приложения с чистого листа
     - откорректировал entrypoint.sh, докерфайл, readme

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -1,22 +1,39 @@
 #!/bin/sh
 
+# ====== CREATE NEW PROJECT IF IT NOT EXIST ======
 IS_NEW_PROJECT=0
-if ! [ -d "$APP_DIR/$APP_FOLDER" ]; then
-  echo "'$APP_FOLDER' does not exist. A new project will be create in the '$APP_FOLDER' directory"
+APP_FOLDER_PATH=$APP_DIR/$APP_FOLDER
+if ! [ -d "$APP_FOLDER_PATH" ]; then
+  echo "'$APP_FOLDER_PATH' does not exist. A new django project will be create in the '$APP_FOLDER_PATH' directory"
   django-admin startproject $APP_FOLDER
   IS_NEW_PROJECT=1
 else
-  echo "'$APP_FOLDER' is exists. Continuing"
+  echo "'$APP_FOLDER_PATH' is exists. Continuing"
 fi
 
-cd $APP_DIR/$APP_FOLDER
-
-if ! [ -f "manage.py" ]; then
+# ====== CHECK IF manage.py file in application directory ======
+if ! [ -f "$APP_FOLDER_PATH/manage.py" ]; then
   echo "Incorrect directory! The file 'manage.py' is not here!"
-  echo "Please check the '$APP_DIR/$APP_FOLDER' directory."
+  echo "Please check the '$APP_FOLDER_PATH' directory."
   echo "EXIT :("
   exit 1
 fi
+
+# ====== CREATE A STATIC DIRECTORY IF IT NOT EXIST ======
+STATIC_FOLDER_PATH=$APP_DIR/$APP_FOLDER/$STATIC_FILES_FOLDER
+if ! [ -d "$STATIC_FOLDER_PATH" ]; then
+  echo "create static at $STATIC_FOLDER_PATH"
+  mkdir $STATIC_FOLDER_PATH
+fi
+
+# ====== CREATE A MEDIA DIRECTORY IF IT NOT EXIST ======
+MEDIA_FOLDER_PATH=$APP_DIR/$APP_FOLDER/$MEDIA_FILES_FOLDER
+if ! [ -d "$MEDIA_FOLDER_PATH" ]; then
+  echo "create media at $MEDIA_FOLDER_PATH"
+  mkdir $MEDIA_FOLDER_PATH
+fi
+
+cd $APP_DIR/$APP_FOLDER
 
 sleep 10
 
