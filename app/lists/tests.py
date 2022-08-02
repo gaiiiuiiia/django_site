248241f2426bca54046972117b6d2f7b3456a2d2
maksim.titov <gaiiiuiiialikeaboss@gmail.com>
2022-08-02 07:45:08 +0300

    todo_app - редирект после сохранения, разделил тесты проверки сохранения и редиректа

--- app/lists/tests.py
+++ app/lists/tests.py
@@ -12,14 +12,17 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'lists/home.html')
 
     def test_can_save_a_POST_request(self) -> None:
-        response: HttpResponse = self.client.post('/', data={'item_text': 'A new list item'})
+        self.client.post('/', data={'item_text': 'A new list item'})
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'lists/home.html')
+    def test_redirect_after_POST(self) -> None:
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
 
 
 class ItemModelTest(TestCase):
--- app/lists/views.py
+++ app/lists/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import redirect
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpRequest
@@ -6,14 +7,9 @@ from lists.models import Item
 
 
 def home_page(request: HttpRequest) -> HttpResponse:
-
-    new_item_text = ''
     if request.method == 'POST':
         new_item_text = request.POST.get('item_text')
         Item.objects.create(text=new_item_text)
+        return redirect('home')
 
-    context = {
-        'new_item_text': new_item_text,
-    }
-
-    return render(request, 'lists/home.html', context)
+    return render(request, 'lists/home.html')
