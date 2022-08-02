8772a1291027109575751a8fe685223ec72c0232
maksim.titov <gaiiiuiiialikeaboss@gmail.com>
2022-08-02 07:28:12 +0300

    todo_app - сохранение в таблицу при запросе домашней страницы

--- app/lists/views.py
+++ app/lists/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpRequest
 
+from lists.models import Item
+
 
 def home_page(request: HttpRequest) -> HttpResponse:
+
+    new_item_text = ''
+    if request.method == 'POST':
+        new_item_text = request.POST.get('item_text')
+        Item.objects.create(text=new_item_text)
+
     context = {
-        'new_item_text': request.POST.get('item_text'),
+        'new_item_text': new_item_text,
     }
 
     return render(request, 'lists/home.html', context)
