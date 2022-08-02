63e7903bb3fe06a68a31385c8787907834257933
maksim.titov <gaiiiuiiialikeaboss@gmail.com>
2022-08-02 05:42:58 +0300

    todo_app - вынес URL сервера в константу

--- app/test/functional_test.py
+++ app/test/functional_test.py
@@ -1,9 +1,12 @@
+import os
 import time
 import unittest
 
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
+
+DOCKER_WEBSERVER_URL = f"http://{os.environ.get('CONTAINER_PREFIX', '')}-webserver"
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -22,7 +25,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self) -> None:
         # Эдит слышала про крутое web приложение, напоминающее список неотложных дел
         # Она заходит на главную страницу и видит, что заголовок сайта говорит ей о списках неотложных дел
-        self._browser.get('http://d-webserver')
+        self._browser.get(DOCKER_WEBSERVER_URL)
         self.assertIn('To-Do List', self._browser.title)
 
         header_text = self._browser.find_element(By.TAG_NAME, 'h1').text
