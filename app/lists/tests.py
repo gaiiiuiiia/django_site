ddd7934bfbbb61d548457432ff0bdb4e8d28cd6e
maksim.titov <gaiiiuiiialikeaboss@gmail.com>
2022-08-02 07:05:51 +0300

    todo_app - добавил модель Item, миграции, тесты

--- app/lists/tests.py
+++ app/lists/tests.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.test import TestCase
 
+from lists.models import Item
+
 
 class HomePageTest(TestCase):
     """Тест домашней страницы"""
@@ -13,3 +15,20 @@ class HomePageTest(TestCase):
         response: HttpResponse = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'lists/home.html')
+
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self) -> None:
+        first_item = Item()
+        first_item.text = 'First item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Second item'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        self.assertEqual(saved_items[0].text, 'First item')
+        self.assertEqual(saved_items[1].text, 'Second item')
